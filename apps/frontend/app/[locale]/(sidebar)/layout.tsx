8d98892d1064c7ae840ee9cbd9a894579795890f
James Zhang <zhjch05@live.com>
2025-07-13 12:33:46 +0800

    Update layout.tsx

--- apps/frontend/app/[locale]/(sidebar)/layout.tsx
+++ apps/frontend/app/[locale]/(sidebar)/layout.tsx
@@ -187,7 +187,7 @@ export default function SidebarLayout({
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+            <SidebarTrigger className="ml-1 cursor-pointer" />
             <Separator orientation="vertical" className="mr-2 h-4" />
           </div>
         </header>
